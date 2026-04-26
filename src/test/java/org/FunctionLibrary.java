3f3eb1dfced34b34662225121ddac3b4dd537646
haussmann <aziznehal1@gmail.com>
2026-04-26 21:52:01 +0200

    hi new

---
+++ src/test/java/org/FunctionLibrary.java
@@ -0,0 +1,57 @@
+package org;
+
+import com.github.javafaker.Faker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class FunctionLibrary {
+
+    private static final Logger log = LogManager.getLogger(FunctionLibrary.class);
+
+    ChromeDriver driver;
+    WebDriverWait wait;
+    Faker faker;
+
+    public FunctionLibrary(ChromeDriver driver) {
+        this.driver = driver;
+        this.wait   = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.faker  = new Faker();
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            log.error("Interruption pendant sleep : {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void waitForElementPresent(WebElement element) {
+        log.debug("Attente de la visibilité de l'élément...");
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOf(element)));
+    }
+    public String generateFakeProductName() {
+        faker = new Faker();
+
+        return faker.commerce().productName() + "_" + System.currentTimeMillis();
+    }
+    public String generateFakeProductCode() {
+        faker = new Faker();
+        return faker.code().ean8() + "_" + System.currentTimeMillis();
+    }
+    public String generateFakeStockLevel() {
+        faker = new Faker();
+        return  String.valueOf(faker.number().numberBetween(1, 100));
+    }
+    public void waitForAlertPresent() {
+        log.debug("Attente de l'alerte...");
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+}
---
+++ src/test/java/org/ProductsPage.java
@@ -0,0 +1,108 @@
+package org;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+/**
+ * @created : 25/04/2026,17:58,sam.
+ **/
+public class ProductsPage {
+    ChromeDriver driver;
+    FunctionLibrary functionLibrary;
+    private String productName;
+    @FindBy(linkText = "Add Product")
+    WebElement addProductLink;
+    @FindBy(id = "name")
+    WebElement productNameField;
+    @FindBy(id = "product_code")
+    WebElement productCodeField;
+    @FindBy(id = "stock_level")
+    WebElement stockLevelField;
+    @FindBy(xpath = "//input[@value=\"Save\"]")
+    WebElement saveButton;
+    @FindBy(xpath ="//*[contains(text(),\"Product successfully created.\")]" )
+    WebElement productAddSuccessMessage;
+    @FindBy(css ="i.fa-trash" )
+    WebElement deleteIcon;
+    @FindBy(xpath ="//*[contains(text(),\"Product(s) successfully deleted.\")]" )
+    WebElement getProductDeleteSuccessMessage;
+
+
+
+    public ProductsPage(ChromeDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        functionLibrary = new FunctionLibrary(driver);
+    }
+    public void addProduct() {
+        functionLibrary.waitForElementPresent(addProductLink);
+        addProductLink.click();
+        productName = functionLibrary.generateFakeProductName();
+        productNameField.sendKeys(productName);
+        productCodeField.sendKeys(functionLibrary.generateFakeProductCode());
+        stockLevelField.sendKeys(functionLibrary.generateFakeStockLevel());
+        saveButton.click();
+
+    }
+    public boolean isProductAddedSuccessfully() {
+        functionLibrary.waitForElementPresent(productAddSuccessMessage);
+//        return functionLibrary.isElementDisplayed(productAddSuccessMessage);
+        if (productAddSuccessMessage.isDisplayed()) {
+            System.out.println("Product is added successfully");
+            return true;
+        } else {
+            System.out.println("Product is not added successfully");
+            return false;
+        }
+
+    }
+
+        public void deleteLastProduct() {
+            List<WebElement> allDeleteIcons = driver.findElements(By.cssSelector("i.fa-trash"));
+            WebElement lastDeleteIcon = allDeleteIcons.get(allDeleteIcons.size() - 1);
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", lastDeleteIcon);
+            functionLibrary.waitForElementPresent(lastDeleteIcon);
+            lastDeleteIcon.click();
+            functionLibrary.waitForAlertPresent();
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+        }
+
+        public void deleteProduct() {
+            functionLibrary.waitForElementPresent(deleteIcon);
+            deleteIcon.click();
+            functionLibrary.waitForAlertPresent();
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+        }
+        public void deleteOwnProduct() {
+        WebElement deleteIcon = driver.findElement(By.xpath(String.format("//*[contains(text(),'%s')]//following::td[10]//a[contains(@class,'delete')]", productName)));
+        deleteIcon.click();
+        functionLibrary.waitForAlertPresent();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        }
+
+
+        public boolean verifyProductDelete() {
+        functionLibrary.waitForElementPresent(getProductDeleteSuccessMessage);
+         if (getProductDeleteSuccessMessage.isDisplayed() ){
+            System.out.println("Product is deleted successfully");
+            return true;
+        } else {
+            System.out.println("Product is not deleted successfully");
+            return false;
+        }
+        }
+
+
+
+}
+
